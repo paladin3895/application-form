74a4ba2943c09a1a07f72cfc286d095968ebc7b6
dat <dat@coachingcloud.com>
2018-03-08 00:38:15 +1300

    implement db models

---
+++ app/Models/Token.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Token extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['email'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * Function to generate a token
+     *
+     * @return string
+     */
+    protected static function generateToken()
+    {
+        return uniqid();
+    }
+
+    public function setEmailAttribute($value) {
+        $this->attributes['email'] = $value;
+        $this->attributes['token'] = static::generateToken();
+        $this->attributes['expired_at'] = new \DateTime('now +' . config('app.token_expire_time') . ' hour');
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['firstname', 'lastname', 'email', 'phone', 'dob', 'gender', 'address'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
